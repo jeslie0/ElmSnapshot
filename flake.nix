a2fc2ea34ec28745bdf3b9ad43e80ff81d660595
James Leslie <jamesleslie@posteo.net>
2024-01-27 12:41:08 +0000

    Remove dependency on flake-utils

--- flake.nix
+++ flake.nix
@@ -3,40 +3,61 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        haskellPackages = pkgs.haskellPackages;
-        homepage = "https://github.com/jeslie0/elmSnapshot";
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems =
+        [ "aarch64-linux" "x86_64-linux" "aarch64-darwin" "x86_64-darwin" ];
+
+      forAllSystems =
+        nixpkgs.lib.genAttrs supportedSystems;
+
+      nixpkgsFor = forAllSystems (system:
+        import nixpkgs {
+          inherit system;
+        });
+
+      haskellPackages =
+        system: nixpkgsFor.${system}.haskellPackages;
+
+      homepage =
+        "https://github.com/jeslie0/elmSnapshot";
       in
         {
-          packages.default = (import ./default.nix (haskellPackages // { lib = pkgs.lib;})) // {
-            meta = {
-              description = "A program to serialise an elm-packages.json to binary";
-              homepage = homepage;
-              license = pkgs.lib.licenses.bsd3;
-            };
-          };
+          packages =
+            forAllSystems (system:
+              let
+                pkgs =
+                  nixpkgsFor.${system};
+              in
+                {
+                  default = (import ./default.nix ((haskellPackages system) // { lib = pkgs.lib;})) // {
+                    meta = {
+                      description = "A program to serialise an elm-packages.json to binary";
+                      homepage = homepage;
+                      license = pkgs.lib.licenses.bsd3;
+                    };
+                  };
+                }
+            );
 
           # Requires IFD.
           # packages.snapshot = haskellPackages.callCabal2nix "snapshot" ./src/snapshot { };
 
-          devShell = haskellPackages.shellFor {
-            packages = p: [
-              self.packages.${system}.default
-            ];
-            nativeBuildInputs = with haskellPackages;
-              [
-              ];
+          devShell =
+            forAllSystems (system:
+              (haskellPackages system).shellFor {
+                packages = p: [
+                  self.packages.${system}.default
+                ];
+                nativeBuildInputs = with haskellPackages;
+                  [
+                  ];
 
-            # Enables Hoogle for the builtin packages.
-            withHoogle = true;
-          };
-        }
-    );
+                # Enables Hoogle for the builtin packages.
+                withHoogle = true;
+              }
+            );
+        };
 }
